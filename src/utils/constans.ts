811442a8f45018cb7f6cb63538946f6a28d36bb9
posop1 <artur.yakhin52@gmail.com>
2022-12-04 20:31:25 +0500

    add links  contans

---
+++ src/utils/constans.ts
@@ -0,0 +1,8 @@
+import { ILinks } from '../types'
+
+export const links: ILinks[] = [
+  { to: '/', name: 'Home' },
+  { to: '/shop', name: 'Shop' },
+  { to: '/faq', name: 'FAQ' },
+  { to: '/about', name: 'About' }
+]

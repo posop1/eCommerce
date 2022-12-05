003e5900835b1e2229860819b149f060c22d55ae
posop1 <artur.yakhin52@gmail.com>
2022-12-05 16:57:52 +0500

    add  FAQ Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes } from 'react-router'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import { HomePage } from './pages/'
+import AboutPage from './pages/AboutPage/AboutPage'
+import FaqPage from './pages/FaqPage/FaqPage'
 
 const App = () => {
   return (
@@ -14,6 +16,14 @@ const App = () => {
           <Route
             index
             element={<HomePage />}
+          />
+          <Route
+            path="faq"
+            element={<FaqPage />}
+          />
+          <Route
+            path="about"
+            element={<AboutPage />}
           />
         </Route>
       </Routes>
---
+++ src/pages/FaqPage/FaqPage.tsx
@@ -0,0 +1,35 @@
+import s from './FaqPage.module.scss'
+
+interface FaqPageProps {}
+
+const FaqPage: React.FC<FaqPageProps> = () => {
+  return (
+    <div className={s.FaqPage}>
+      <h3>FAQ</h3>
+      <ul>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, doloremque quos quaerat
+          reiciendis facere provident quod est nostrum ipsam molestias sit nisi, illum illo alias
+          deserunt beatae iure dolores nam?
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, doloremque quos quaerat
+          reiciendis facere provident quod est nostrum ipsam molestias sit nisi, illum illo alias
+          deserunt beatae iure dolores nam?
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, doloremque quos quaerat
+          reiciendis facere provident quod est nostrum ipsam molestias sit nisi, illum illo alias
+          deserunt beatae iure dolores nam?
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, doloremque quos quaerat
+          reiciendis facere provident quod est nostrum ipsam molestias sit nisi, illum illo alias
+          deserunt beatae iure dolores nam?
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default FaqPage

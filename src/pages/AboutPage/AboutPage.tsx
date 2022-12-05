253f882726668537a26b8b98c58632d28d6f5c75
posop1 <artur.yakhin52@gmail.com>
2022-12-05 16:57:38 +0500

    add About Page

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,23 @@
+import s from './AboutPage.module.scss'
+
+interface AboutPageProps {}
+
+const AboutPage: React.FC<AboutPageProps> = () => {
+  return (
+    <div className={s.AboutPage}>
+      <h3>About</h3>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem adipisci minima
+        eos! Debitis atque quae iusto quod. Expedita totam et ex repudiandae in soluta consequatur
+        pariatur, neque ullam, blanditiis odio! Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Exercitationem adipisci minima eos! Debitis atque quae iusto quod. Expedita totam et
+        ex repudiandae in soluta consequatur pariatur, neque ullam, blanditiis odio! Lorem, ipsum
+        dolor sit amet consectetur adipisicing elit. Exercitationem adipisci minima eos! Debitis
+        atque quae iusto quod. Expedita totam et ex repudiandae in soluta consequatur pariatur,
+        neque ullam, blanditiis odio!
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage

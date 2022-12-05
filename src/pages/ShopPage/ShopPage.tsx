4857ed8151bb1555ee39c265bf50f191ab139d37
posop1 <artur.yakhin52@gmail.com>
2022-12-05 17:03:35 +0500

    add shop Page template

---
+++ src/pages/ShopPage/ShopPage.tsx
@@ -0,0 +1,9 @@
+import s from './ShopPage.module.scss'
+
+interface ShopPageProps {}
+
+const ShopPage: React.FC<ShopPageProps> = () => {
+  return <div className={s.ShopPage}>shop</div>
+}
+
+export default ShopPage

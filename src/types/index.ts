cdee7caebe46b3e2a64c66979a61ac4e2347169c
posop1 <artur.yakhin52@gmail.com>
2022-12-06 21:15:31 +0500

    add types for products

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,12 @@ export interface IRoutes {
   path: string
   element: React.FC
 }
+
+export interface IProduct {
+  id: number
+  name: string
+  description: string
+  price: number
+  image: string
+  category: string
+}

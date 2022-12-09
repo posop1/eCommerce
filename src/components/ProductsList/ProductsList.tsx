7d3fdc7ade506bf929491a577a1c24f0fd5415f0
posop1 <artur.yakhin52@gmail.com>
2022-12-09 14:43:06 +0500

    add productList component

---
+++ src/components/ProductsList/ProductsList.tsx
@@ -0,0 +1,26 @@
+import { IProduct } from '../../types'
+import { ProductItem } from '../ProductItem/ProductItem'
+import s from './ProductsList.module.scss'
+
+interface ProductListProps {
+  products?: IProduct[]
+}
+
+export const ProductsList: React.FC<ProductListProps> = ({ products }) => {
+  return (
+    <div className={s.ProductsList}>
+      {products &&
+        products.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            name={product.name}
+            description={product.description}
+            price={product.price}
+            image={product.image}
+            category={product.category}
+          />
+        ))}
+    </div>
+  )
+}

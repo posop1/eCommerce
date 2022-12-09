ccf4a7bb7a4c9326234b1e2e380bcf081cd6e3dd
posop1 <artur.yakhin52@gmail.com>
2022-12-09 19:06:01 +0500

    add cart page total info

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,44 +1,74 @@
+import { Link } from 'react-router-dom'
 import useActions from '../../hooks/useActions'
 import { useAppSelector } from '../../hooks/useAppSelector'
+import { calcTotalPrice } from '../../utils/calcTotalProce'
 import s from './CartPage.module.scss'
 
 export const CartPage: React.FC = () => {
   const { removeItem } = useActions()
   const { cart } = useAppSelector((state) => state)
 
+  const totalPrice = calcTotalPrice(cart)
+
   return (
     <div className={s.CartPage}>
       <div className="container">
         <div className={s.inner}>
           <div className={s.cart__list}>
-            {cart.map((item) => (
-              <div
-                key={item.id}
-                className={s.cart__item}
-              >
-                <div className={s.cart__item__info}>
-                  <img
-                    src={item.image}
-                    alt="cart image"
-                    className={s.image}
-                  />
-                  <div>
-                    <h3>{item.name}</h3>
-                    <span>{item.category}</span>
-                    <p>{item.description}</p>
-                    <button
-                      className={s.btn}
-                      onClick={() => removeItem({ id: item.id })}
-                    >
-                      Remove
-                    </button>
+            {cart.length > 0 ? (
+              cart.map((item) => (
+                <div
+                  key={item.id}
+                  className={s.cart__item}
+                >
+                  <div className={s.cart__item__info}>
+                    <img
+                      src={item.image}
+                      alt="cart image"
+                      className={s.image}
+                    />
+                    <div>
+                      <h3>{item.name}</h3>
+                      <span>{item.category}</span>
+                      <p>{item.description}</p>
+                      <button
+                        className={s.btn}
+                        onClick={() => removeItem({ id: item.id })}
+                      >
+                        Remove
+                      </button>
+                    </div>
                   </div>
+                  <span className={s.info__price}>{item.price}$</span>
                 </div>
-                <span className={s.info__price}>{item.price}$</span>
+              ))
+            ) : (
+              <div className={s.empty}>
+                <h3>Cart is Empty</h3>
               </div>
-            ))}
+            )}
           </div>
-          <div className={s.cart__info}>info</div>
+          {cart.length > 0 && (
+            <div className={s.cart__info}>
+              <h3>Cart</h3>
+              <div className={s.info__list}>
+                {cart.map((item) => (
+                  <div key={item.id}>
+                    <p>{item.name}</p>
+                    <span>{item.price}$</span>
+                  </div>
+                ))}
+                <hr />
+                <span className={s.total}>Total: {totalPrice}$</span>
+              </div>
+              <Link
+                to="/u-can-not-buy"
+                className={s.buy__btn}
+              >
+                Buy
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </div>

fdcc0fb31dacdca0acb4204c0c84fb0aad5e8023
posop1 <artur.yakhin52@gmail.com>
2022-12-09 15:58:36 +0500

    add go to cart component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from 'react-router-dom'
+import { useAppSelector } from '../../hooks/useAppSelector'
 import '../../styles/app.scss'
+import { GoToCart } from '../GoToCart/GoToCart'
 import Footer from './Footer/Footer'
 import Navbar from './Navbar/Navbar'
 
 interface LayoutProps {}
 
 const Layout: React.FC<LayoutProps> = () => {
+  const { cart } = useAppSelector((state) => state)
+
   return (
     <div className="app">
       <Navbar />
+      {cart.length > 0 && <GoToCart />}
       <div className="main">
         <Outlet />
       </div>

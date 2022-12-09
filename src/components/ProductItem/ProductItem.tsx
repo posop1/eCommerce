a2274bcd4ee1d9e7fdc520810272d443a927e266
posop1 <artur.yakhin52@gmail.com>
2022-12-09 15:28:01 +0500

    add cart item functional

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -1,20 +1,41 @@
+import { Link } from 'react-router-dom'
+import useActions from '../../hooks/useActions'
+import { useAppSelector } from '../../hooks/useAppSelector'
 import { IProduct } from '../../types'
 import s from './ProductItem.module.scss'
 
-export const ProductItem: React.FC<IProduct> = ({ name, price, image, category, id }) => {
+interface ProductItemProps {
+  product: IProduct
+}
+
+export const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
+  const { addItem } = useActions()
+  const { removeItem } = useActions()
+  const { cart } = useAppSelector((state) => state)
+
+  const isExistsInCart = cart.some((i) => i.id === product.id)
+
   return (
     <div className={s.ProductItem}>
-      <img
-        src={image}
-        alt=""
-        className={s.image}
-      />
+      <Link to="/shop">
+        <img
+          src={product.image}
+          alt=""
+          className={s.image}
+        />
+      </Link>
+
       <div className={s.info}>
-        <span className={s.name}>{name}</span>
-        <span className={s.price}>{price}$</span>
+        <span className={s.name}>{product.name}</span>
+        <span className={s.price}>{product.price}$</span>
       </div>
-      <span className={s.category}>{category}</span>
-      <button className={s.btn}>Buy</button>
+      <span className={s.category}>{product.category}</span>
+      <button
+        className={s.btn}
+        onClick={() => (isExistsInCart ? removeItem({ id: product.id }) : addItem(product))}
+      >
+        {isExistsInCart ? 'Remove in Cart' : 'Add to Cart'}
+      </button>
     </div>
   )
 }

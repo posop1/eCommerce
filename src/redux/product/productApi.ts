d8a37d966acc0918d9cbe92a7431b033bbc2bc9e
posop1 <artur.yakhin52@gmail.com>
2022-12-09 22:24:47 +0500

    fix product api shit error

--- src/redux/product/productApi.ts
+++ src/redux/product/productApi.ts
@@ -4,9 +4,9 @@ import { IProduct } from '../../types'
 export const productApi = createApi({
   reducerPath: 'api/products',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://638f65344ddca317d7f7732e.mockapi.io/api/' }),
-  endpoints: (bulid) => ({
-    fetchProducts: bulid.query<IProduct[], number>({
-      query: (limit) => `products?limit=${limit}`
+  endpoints: (build) => ({
+    fetchProducts: build.query<IProduct[], number>({
+      query: (limit) => `/products?page=1&limit=${limit}`
     })
   })
 })

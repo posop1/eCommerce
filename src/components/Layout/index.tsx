fc5ee23e2386b56e95e2002bcf8e7f814c81ad70
posop1 <artur.yakhin52@gmail.com>
2022-12-09 19:06:48 +0500

    fix go to cart visiblity

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { useAppSelector } from '../../hooks/useAppSelector'
 import '../../styles/app.scss'
 import { GoToCart } from '../GoToCart/GoToCart'
@@ -9,11 +9,12 @@ interface LayoutProps {}
 
 const Layout: React.FC<LayoutProps> = () => {
   const { cart } = useAppSelector((state) => state)
+  const location = useLocation()
 
   return (
     <div className="app">
       <Navbar />
-      {cart.length > 0 && <GoToCart />}
+      {cart.length > 0 && location.pathname !== '/cart' && <GoToCart />}
       <div className="main">
         <Outlet />
       </div>

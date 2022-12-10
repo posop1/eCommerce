d8a879b22bf69e6b4cf4d529410b54d350b3c56e
posop1 <artur.yakhin52@gmail.com>
2022-12-10 13:44:55 +0500

    add product list in shop page

--- src/pages/ShopPage/ShopPage.tsx
+++ src/pages/ShopPage/ShopPage.tsx
@@ -1,7 +1,56 @@
+import { useState } from 'react'
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai'
+import { InfinitySpin } from 'react-loader-spinner'
+import { ProductsList } from '../../components/ProductsList/ProductsList'
+import { useFetchProductsQuery } from '../../redux/product/productApi'
 import s from './ShopPage.module.scss'
 
 interface ShopPageProps {}
 
 export const ShopPage: React.FC<ShopPageProps> = () => {
-  return <div className={s.ShopPage}>shop</div>
+  const [page, setPage] = useState(1)
+  const { data = [], isLoading, error } = useFetchProductsQuery({ page })
+
+  const incrementPage = () => {
+    setPage((prev) => prev + 1)
+  }
+  const decrementPage = () => {
+    if (page === 1) {
+      return
+    }
+    setPage((prev) => prev - 1)
+  }
+
+  return (
+    <div className={s.ShopPage}>
+      <div className="container">
+        <div className={s.inner}>
+          <div className={s.params}></div>
+        </div>
+        <div className={s.products}>
+          {isLoading ? (
+            <div className={s.spinner}>
+              <InfinitySpin
+                width="200"
+                color="#080808"
+              />
+            </div>
+          ) : error ? (
+            <div className={s.error}>ERROR</div>
+          ) : (
+            <ProductsList products={data} />
+          )}
+        </div>
+      </div>
+      <div className={s.pagination}>
+        <button onClick={decrementPage}>
+          <AiOutlineLeft size={20} />
+        </button>
+        <span>{page}</span>
+        <button onClick={incrementPage}>
+          <AiOutlineRight size={20} />
+        </button>
+      </div>
+    </div>
+  )
 }
